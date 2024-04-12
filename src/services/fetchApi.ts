4a5f32e0b7b645e2e3554d8b250713258ea09c23
Carlos Eduardo <cpwaldow@gmail.com>
2024-04-12 15:21:13 -0300

    feat: fetch riot api

---
+++ src/services/fetchApi.ts
@@ -0,0 +1,8 @@
+export const fetchChampionsList = async () => {
+  const API_URL =
+    'https://ddragon.leagueoflegends.com/cdn/14.4.1/data/pt_BR/champion.json';
+  const response = await fetch(API_URL);
+  const data = await response.json();
+
+  return data;
+};
