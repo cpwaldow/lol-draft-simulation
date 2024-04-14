03d704cb068fef17c2715e417ee47e8a2d80658d
Carlos Eduardo <cpwaldow@gmail.com>
2024-04-14 09:24:14 -0300

    refactor: add store.dispatch in fetchapi functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
 import { useEffect } from 'react';
 import './App.css';
 import Title from './components/Title';
-import store from './redux/store';
 import { fetchChampionsList } from './services/fetchApi';
 
 function App() {
   useEffect(() => {
-    const handleFetch = async () => {
-      const champions = await fetchChampionsList();
-      store.dispatch({ type: 'SET_CHAMPIONS', payload: champions });
-    };
-    handleFetch();
+    fetchChampionsList();
   }, []);
 
   return (
--- src/services/fetchApi.ts
+++ src/services/fetchApi.ts
@@ -1,8 +1,9 @@
+import store from '../redux/store';
+
 export const fetchChampionsList = async () => {
   const API_URL =
     'https://ddragon.leagueoflegends.com/cdn/14.4.1/data/pt_BR/champion.json';
   const response = await fetch(API_URL);
   const data = await response.json();
-
-  return data;
+  store.dispatch({ type: 'SET_CHAMPIONS', payload: data });
 };
