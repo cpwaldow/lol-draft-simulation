f73f3b4eb898a44c2b7f18f59a7c688667b4c0de
Carlos Eduardo <cpwaldow@gmail.com>
2024-04-14 09:12:31 -0300

    feat: fetch champions API and set to redux store

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,18 @@
+import { useEffect } from 'react';
 import './App.css';
 import Title from './components/Title';
+import store from './redux/store';
+import { fetchChampionsList } from './services/fetchApi';
+
 function App() {
+  useEffect(() => {
+    const handleFetch = async () => {
+      const champions = await fetchChampionsList();
+      store.dispatch({ type: 'SET_CHAMPIONS', payload: champions });
+    };
+    handleFetch();
+  }, []);
+
   return (
     <>
       <Title />
