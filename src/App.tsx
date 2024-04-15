b3895808459991cb5ba01bc54b96a4606d6f47aa
Carlos Eduardo <cpwaldow@gmail.com>
2024-04-15 11:02:51 -0300

    refactor: dispatch champions fetch sending at App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,14 @@ import { useEffect } from 'react';
 import './App.css';
 import Title from './components/Title';
 import { fetchChampionsList } from './services/fetchApi';
+import store from './redux/store';
 
 function App() {
   useEffect(() => {
-    fetchChampionsList();
+    (async () => {
+      const champions = await fetchChampionsList();
+      store.dispatch({ type: 'SET_CHAMPIONS', payload: champions });
+    })();
   }, []);
 
   return (
--- src/services/fetchApi.ts
+++ src/services/fetchApi.ts
@@ -1,9 +1,7 @@
-import store from '../redux/store';
-
 export const fetchChampionsList = async () => {
   const API_URL =
     'https://ddragon.leagueoflegends.com/cdn/14.4.1/data/pt_BR/champion.json';
   const response = await fetch(API_URL);
   const data = await response.json();
-  store.dispatch({ type: 'SET_CHAMPIONS', payload: data });
+  return data;
 };
